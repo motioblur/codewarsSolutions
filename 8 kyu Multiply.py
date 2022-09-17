a3a883a1f2d1b2808abae48e9f968fdbc67da6f3
Carlos Vite <54517058+motioblur@users.noreply.github.com>
2022-09-16 21:26:11 -0500

    Add files via upload

---
+++ 8 kyu Multiply.py
@@ -0,0 +1,10 @@
+# This Kata can be found at https://www.codewars.com/kata/50654ddff44f800200000004
+#
+# Description:
+#
+# This code does not execute properly. Try to figure out why.
+#
+# Solution:
+
+def multiply(a, b):
+  return a * b
